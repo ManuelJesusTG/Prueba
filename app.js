24e3fd73e4fe38b51b40231866e003a7fa6c2e41
ManuelJesusTG <90841523+ManuelJesusTG@users.noreply.github.com>
2023-12-11 16:23:20 +0100

    prueba

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const cors = require('cors');
